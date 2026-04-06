e1cc069fa67849890c2dd45b25b373d106da6dd0
DomenicoFoglia <foglia.dmnc@gmail.com>
2026-04-07 00:20:36 +0200

    Fix: Supporto ODT per lettura CV

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[Fillable(['name', 'email', 'password', 'theme', 'cv_text'])]
+#[Fillable(['name', 'email', 'password', 'theme'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
